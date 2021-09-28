d7a264909bfbb7f9058ef853f6365d72736c0de6
Mithun Das <mithun.das@coxautoinc.com>
2021-09-28 14:43:53 -0400

    changing variable value

--- ei-processing/app/ei_run.sh
+++ ei-processing/app/ei_run.sh
@@ -2,9 +2,9 @@
 
 wget https://raw.githubusercontent.com/just4give/birdwatcher/master/data/birds.json 
 
-if [ $EI_COLLECT_MODE_IMAGE = "1" ];
+if [ $EI_COLLECT_MODE_IMAGE = "Y" ];
 then
-    edge-impulse-linux --api-key $EI_API_KEY_IMAGE
+    edge-impulse-linux --api-key $EI_API_KEY_IMAGE --disable-microphone
 else
     edge-impulse-linux-runner --api-key $EI_API_KEY_IMAGE --download modelfile.eim
     python3 classify.py 
